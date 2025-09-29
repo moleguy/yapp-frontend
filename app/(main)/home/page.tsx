0692912c36df7e0cc1f1e681bc32f70d435b918a
moleguy <tamangmanish446@gmail.com>
2025-09-29 19:51:29 +0545

    added mock test for server friends

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -93,6 +93,10 @@ export default function HomePage() {
     },
   ]);
 
+  // Separate online and offline friends
+  const onlineFriends = friends.filter(friend => friend.status === "online");
+  const offlineFriends = friends.filter(friend => friend.status === "offline");
+
   const handleServerClick = (server: Server) => {
     setActiveServer(server);
     setActiveView("server");
@@ -116,9 +120,9 @@ export default function HomePage() {
 
         if (newServers.length > 0) {
           const nextIndex =
-            serverIndex < newServers.length
-              ? serverIndex
-              : newServers.length - 1;
+              serverIndex < newServers.length
+                  ? serverIndex
+                  : newServers.length - 1;
           nextActiveServer = newServers[nextIndex];
         }
         setActiveServer(nextActiveServer);
@@ -158,162 +162,220 @@ export default function HomePage() {
   };
 
   return (
-    <ProtectedRoute>
-      <div className="flex h-screen bg-black text-black font-MyFont">
-        <div className="relative w-full flex bg-[#EAE4D5] m-1">
-          {/* Sidebar */}
-          <div className="flex flex-col h-full w-[350px] bg-[#f3f3f4] rounded-l-lg">
-            {/* Server list */}
-            <ServerList
-              servers={servers}
-              setServers={setServers}
-              activeServer={activeServer}
-              onServerClick={handleServerClick}
-              onLeaveServer={handleLeaveServer}
-              onDirectMessagesClick={handleDirectMessageClick}
-              onServersToggle={handleServersTabClick}
-              activeView={activeView}
-              onCreateCategoryClick={handleCreateCategoryClick}
-            />
+      <ProtectedRoute>
+        <div className="flex h-screen bg-black text-black font-MyFont">
+          <div className="relative w-full flex bg-[#EAE4D5] m-1">
+            {/* Sidebar */}
+            <div className="flex flex-col h-full w-[350px] bg-[#f3f3f4] rounded-l-lg">
+              {/* Server list */}
+              <ServerList
+                  servers={servers}
+                  setServers={setServers}
+                  activeServer={activeServer}
+                  onServerClick={handleServerClick}
+                  onLeaveServer={handleLeaveServer}
+                  onDirectMessagesClick={handleDirectMessageClick}
+                  onServersToggle={handleServersTabClick}
+                  activeView={activeView}
+                  onCreateCategoryClick={handleCreateCategoryClick}
+              />
 
-            {/* Channels and Friends Section To Be Displayed */}
-            <div
-              className={`flex-1 min-h-0 overflow-y-auto
+              {/* Channels and Friends Section To Be Displayed */}
+              <div
+                  className={`flex-1 min-h-0 overflow-y-auto
               ${activeView === "server" && activeServer ? "border-t border-[#dcd9d3]" : ""}
               `}
-            >
-              {showServersOnly && activeView === "server" && activeServer ? (
-                <ServerDetails
-                  activeServer={activeServer}
-                  onSelectChannel={setSelectedChannel}
-                  showCategoryPopup={showCategoryPopup && categoryPopupServer?.id === activeServer.id}
-                  onCloseCategoryPopup={() => setShowCategoryPopup(false)}
-                  onOpenCategoryPopup={handleOpenCategoryPopup}
-                />
-              ) : activeView === "dm" ? (
-                <DirectMessages
-                  friends={friends}
-                  onSelectFriend={setSelectedFriend}
-                />
-              ) : null}
-            </div>
-
-            {/* Profile with controls */}
-            <div className="flex w-[320px] m-3 py-2 px-2 bg-white border border-[#D4C9BE] rounded-xl select-none items-center justify-between">
-              {/* Left: avatar + names */}
-              <div className="flex items-center">
-                {hasAvatar ? (
-                  <Image
-                    src={avatarThumbnailUrl}
-                    alt="Profile"
-                    className="w-12 h-12 object-cover rounded-full"
-                    width={90}
-                    height={90}
-                    onError={() => {
-                      console.error("Avatar image failed to load:", avatarThumbnailUrl); // REMOVE IN PROD
-                    }}
-                  />
-                ) : (
-                  <div className="w-12 h-12 bg-gray-500 rounded-full flex items-center justify-center text-white font-medium">
-                    {fallback}
-                  </div>
-                )}
-                <div className="ml-2 font-MyFont text-[#393E46]">
-                  <p className="text-sm font-medium">
-                    {user?.display_name || "No Name"}
-                  </p>
-                  <p className="text-sm">@{user?.username || "no-username"}</p>
-                </div>
+              >
+                {showServersOnly && activeView === "server" && activeServer ? (
+                    <ServerDetails
+                        activeServer={activeServer}
+                        onSelectChannel={setSelectedChannel}
+                        showCategoryPopup={showCategoryPopup && categoryPopupServer?.id === activeServer.id}
+                        onCloseCategoryPopup={() => setShowCategoryPopup(false)}
+                        onOpenCategoryPopup={handleOpenCategoryPopup}
+                    />
+                ) : activeView === "dm" ? (
+                    <DirectMessages
+                        friends={friends}
+                        onSelectFriend={setSelectedFriend}
+                    />
+                ) : null}
               </div>
 
-              {/* Right: mic + settings */}
-              <div className="flex items-center gap-2">
-                <div
-                  className={`cursor-pointer flex justify-center items-center p-2 rounded-lg ${
-                    showMicrophone
-                      ? "hover:bg-[#dfdfe1] hover:text-[#1e1e1e]"
-                      : "bg-[#ebc8ca] text-[#cb3b40]"
-                  }
-                    `}
-                  onClick={() => setShowMicrophone(!showMicrophone)}
-                >
-                  {showMicrophone ? (
-                    <button className="cursor-pointer hover:text-[#1e1e1e]">
-                      <BiSolidMicrophone size={24} className="text-gray-500" />
-                    </button>
-                  ) : (
-                    <button>
-                      <BiSolidMicrophoneOff
-                        size={24}
-                        className="cursor-pointer"
+              {/* Profile with controls */}
+              <div className="flex w-[320px] m-3 py-2 px-2 bg-white border border-[#D4C9BE] rounded-xl select-none items-center justify-between">
+                {/* Left: avatar + names */}
+                <div className="flex items-center">
+                  {hasAvatar ? (
+                      <Image
+                          src={avatarThumbnailUrl}
+                          alt="Profile"
+                          className="w-12 h-12 object-cover rounded-full"
+                          width={90}
+                          height={90}
+                          onError={() => {
+                            console.error("Avatar image failed to load:", avatarThumbnailUrl); // REMOVE IN PROD
+                          }}
                       />
-                    </button>
+                  ) : (
+                      <div className="w-12 h-12 bg-gray-500 rounded-full flex items-center justify-center text-white font-medium">
+                        {fallback}
+                      </div>
+                  )}
+                  <div className="ml-2 font-MyFont text-[#393E46]">
+                    <p className="text-sm font-medium">
+                      {user?.display_name || "No Name"}
+                    </p>
+                    <p className="text-sm">@{user?.username || "no-username"}</p>
+                  </div>
+                </div>
+
+                {/* Right: mic + settings */}
+                <div className="flex items-center gap-2">
+                  <div
+                      className={`cursor-pointer flex justify-center items-center p-2 rounded-lg ${
+                          showMicrophone
+                              ? "hover:bg-[#dfdfe1] hover:text-[#1e1e1e]"
+                              : "bg-[#ebc8ca] text-[#cb3b40]"
+                      }
+                    `}
+                      onClick={() => setShowMicrophone(!showMicrophone)}
+                  >
+                    {showMicrophone ? (
+                        <button className="cursor-pointer hover:text-[#1e1e1e]">
+                          <BiSolidMicrophone size={24} className="text-gray-500" />
+                        </button>
+                    ) : (
+                        <button>
+                          <BiSolidMicrophoneOff
+                              size={24}
+                              className="cursor-pointer"
+                          />
+                        </button>
+                    )}
+                  </div>
+
+                  <div className="flex justify-center items-center p-2 rounded-lg hover:bg-[#dfdfe1] cursor-pointer">
+                    <SettingsPopup
+                        isOpen={settingsOpen}
+                        onClose={() => setSettingsOpen(false)}
+                        onOpen={() => setSettingsOpen(true)}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="relative flex flex-2 flex-col justify-between bg-[#fbfbfb] border-r border-[#dcd9d3]">
+              {/* User's information*/}
+              {/*<div>*/}
+              {/*  Username: {user?.username}*/}
+              {/*  <br />*/}
+              {/*  Display name: {user?.display_name}*/}
+              {/*  <br />*/}
+              {/*  Email: {user?.email}*/}
+              {/*  <br />*/}
+              {/*  Avatar URL: {user?.avatar_url ? user?.avatar_url : "Not Set"}*/}
+              {/*  <br />*/}
+              {/*  Active : {user?.active ? "True" : "False"}*/}
+              {/*</div>*/}
+
+              {activeView === "server" && activeServer && selectedChannel ? (
+                  <ChatArea
+                      serverName={activeServer.name}
+                      channelName={selectedChannel.name}
+                      channelId={`server-${activeServer.id}-channel-${selectedChannel.id}`} // Unique ID for each channel
+                      isDm={false}
+                  />
+              ) : activeView === "dm" && selectedFriend ? (
+                  <ChatArea
+                      friendDisplayName={
+                          selectedFriend.display_name || selectedFriend.name
+                      }
+                      friendUsername={selectedFriend.username || ""}
+                      friendId={selectedFriend.id.toString()}
+                      friendAvatar={selectedFriend.avatarUrl}
+                      isDm={true}
+                  />
+              ) : (
+                  <div className="flex-1 flex items-center justify-center text-gray-500">
+                    {activeView === "dm"
+                        ? "Select a friend to start chatting"
+                        : "Select a channel to start chatting"}
+                  </div>
+              )}
+            </div>
+
+            <div className="flex flex-col w-[400px] bg-[#fbfbfb] rounded-r-lg">
+              {/* Friends List Section - Shows in both server and DM views */}
+              <div className="h-full flex flex-col">
+
+                {/* Friends List */}
+                <div className="flex-1 overflow-y-auto ">
+                  {activeView === "server" && activeServer ? (
+                      // Server View - Show online/offline members like Discord
+                      <div className="py-6 px-3">
+                        <label className="text-sm px-3 font-base text-[#73726e] tracking-wide mb-2">
+                          Online — {onlineFriends.length}
+                        </label>
+                        <div className="space-y-2 mb-8">
+                          {onlineFriends.map((friend) => (
+                              <div
+                                  key={friend.id}
+                                  className="flex items-center gap-3 py-2 px-3 rounded-lg cursor-pointer hover:bg-[#e7e7e9] text-[#73726e] hover:text-[#222831]"
+                              >
+                                <div className="relative">
+                                  <div className="w-12 h-12 bg-[#3a6f43] rounded-full flex items-center justify-center text-white text-lg font-medium">
+                                    {friend.name.charAt(0).toUpperCase()}
+                                  </div>
+                                  <div className="absolute -bottom-0 left-8 w-4 h-4 bg-[#08cb00] border-2 border-white rounded-full "></div>
+                                </div>
+                                <div className="flex-1 min-w-0">
+                                  <p className="text-lg font-medium    tracking-wide">
+                                    {friend.name}
+                                  </p>
+                                </div>
+                              </div>
+                          ))}
+                        </div>
+
+                        <label className="text-sm px-3 font-base text-[#73726e]  tracking-wide mb-2">
+                          Offline — {offlineFriends.length}
+                        </label>
+                        <div className="space-y-2">
+                          {offlineFriends.map((friend) => (
+                              <div
+                                  key={friend.id}
+                                  className="flex items-center gap-3 py-2 px-3 rounded-lg hover:bg-[#e7e7e9] hover:opacity-100 cursor-pointer group opacity-30"
+                              >
+                                <div className="relative">
+                                  <div className="w-12 h-12 bg-gray-400 rounded-full flex items-center justify-center text-white text-lg font-medium">
+                                    {friend.name.charAt(0).toUpperCase()}
+                                  </div>
+                                </div>
+                                <div className="flex-1 min-w-0">
+                                  <p className="text-lg font-medium text-[#222831] tracking-wide">
+                                    {friend.name}
+                                  </p>
+                                </div>
+                              </div>
+                          ))}
+                        </div>
+                      </div>
+                  ) : activeView === "dm" ? (
+                      // DM View - Show friend profile
+                      <FriendsProfile friend={selectedFriend} />
+                  ) : (
+                      // Default view when no server or DM is selected
+                      <div className="flex-1 flex items-center justify-center text-gray-500">
+                        Select a server or friend to view details
+                      </div>
                   )}
                 </div>
-
-                <div className="flex justify-center items-center p-2 rounded-lg hover:bg-[#dfdfe1] cursor-pointer">
-                  <SettingsPopup
-                    isOpen={settingsOpen}
-                    onClose={() => setSettingsOpen(false)}
-                    onOpen={() => setSettingsOpen(true)}
-                  />
-                </div>
               </div>
-            </div>
-          </div>
-
-          <div className="relative flex flex-2 flex-col justify-between bg-[#fbfbfb] border-r border-[#dcd9d3]">
-            {/* User's information*/}
-            {/*<div>*/}
-            {/*  Username: {user?.username}*/}
-            {/*  <br />*/}
-            {/*  Display name: {user?.display_name}*/}
-            {/*  <br />*/}
-            {/*  Email: {user?.email}*/}
-            {/*  <br />*/}
-            {/*  Avatar URL: {user?.avatar_url ? user?.avatar_url : "Not Set"}*/}
-            {/*  <br />*/}
-            {/*  Active : {user?.active ? "True" : "False"}*/}
-            {/*</div>*/}
-
-            {activeView === "server" && activeServer && selectedChannel ? (
-              <ChatArea
-                serverName={activeServer.name}
-                channelName={selectedChannel.name}
-                channelId={`server-${activeServer.id}-channel-${selectedChannel.id}`} // Unique ID for each channel
-                isDm={false}
-              />
-            ) : activeView === "dm" && selectedFriend ? (
-              <ChatArea
-                friendDisplayName={
-                  selectedFriend.display_name || selectedFriend.name
-                }
-                friendUsername={selectedFriend.username || ""}
-                friendId={selectedFriend.id.toString()}
-                friendAvatar={selectedFriend.avatarUrl}
-                isDm={true}
-              />
-            ) : (
-              <div className="flex-1 flex items-center justify-center text-gray-500">
-                {activeView === "dm"
-                  ? "Select a friend to start chatting"
-                  : "Select a channel to start chatting"}
-              </div>
-            )}
-          </div>
-
-          <div className="flex flex-col w-[400px] bg-[#fbfbfb] rounded-r-lg">
-            {/* search and friends who are online and offline */}
-
-            {/* Friends Section */}
-            <div>
-              {!showServersOnly && activeView === "dm" && (
-                <FriendsProfile friend={selectedFriend} />
-              )}
             </div>
           </div>
         </div>
-      </div>
-    </ProtectedRoute>
+      </ProtectedRoute>
   );
 }
